e77d8b42ff7e077ba646a11382f252ea30cff657
AshishKhadka <khadkaashish50@gmail.com>
2026-06-07 10:00:00 +0545

    Learning updates

--- React/03_COUNTER_STATE/src/App.jsx
+++ React/03_COUNTER_STATE/src/App.jsx
@@ -1,36 +1,26 @@
+import {useState} from "react";
+
 import "./App.css";
 
 export default function App() {
+  const[] = useState(0);
   return (
     <>
-      <h1>Counter</h1>
+      <div className="container">
+        <h1>Counter</h1>
 
-      <div className="card">Count is 20</div>
+        <div className="card">Count is 20</div>
 
-      <div>
-        <button onClick={() => {}} style={{ margin: "0 5px" }}>
-          Increase
-        </button>{" "}
-        <button onClick={() => {}} style={{ margin: "0 5px" }}>
-          Decrease
-        </button>{" "}
-        <button onClick={() => {}} style={{ margin: "0 5px" }}>
-          Reset
-        </button>
-      </div>
-      <div style={{ margin: "20px 0" }}>
-        <input
-          style={{
-            width: "100px",
-            border: "1px solid white",
-            margin: "0 5px",
-            padding: "0.6em 1.2em",
-          }}
-          value="8"
-          onChange={()=>{}}
-          type="text"
-        />
-        <button>Set to 8</button>
+        <div className="buttons">
+          <button>Increase</button>
+          <button>Decrease</button>
+          <button>Reset</button>
+        </div>
+
+        <div className="input-group">
+          <input type="text" value="8" onChange={() => {}} />
+          <button>Set to 8</button>
+        </div>
       </div>
     </>
   );
