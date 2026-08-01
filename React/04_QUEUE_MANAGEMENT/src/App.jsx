407ca3ef0d1011b62028bd2ddea20260916092c9
AshishKhadka <khadkaashish50@gmail.com>
2026-08-01 21:50:53 +0545

    updated

--- React/04_QUEUE_MANAGEMENT/src/App.jsx
+++ React/04_QUEUE_MANAGEMENT/src/App.jsx
@@ -1,8 +1,13 @@
+import "./App.css";
+
 export default function App() {
-  return(
-    <>
-      <h1>Queue Management System</h1>
-      <p>Manage your customers efficiently</p>
-    </>
-  )
+  return (
+    <div>
+      <header>
+        <h1>Queue Management System</h1>
+        <p>Manage your customers efficiently</p>
+      </header>
+      
+    </div>
+  );
 }
