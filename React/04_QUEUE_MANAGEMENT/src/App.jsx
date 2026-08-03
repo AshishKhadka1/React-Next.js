dcbc32e19346a8d3a5cef541a6d649fc56bd5882
AshishKhadka <khadkaashish50@gmail.com>
2026-08-03 22:59:08 +0545

    Updated

--- React/04_QUEUE_MANAGEMENT/src/App.jsx
+++ React/04_QUEUE_MANAGEMENT/src/App.jsx
@@ -1,14 +1,32 @@
 import {useState} from "react";
 import "./App.css";
+import QueueForm from "./components/QueueForm";
 
 export default function App() {
-  const [] = useState([]);
+  const [queue, setQueue] = useState([]);
+
+const addToQueue = (customer) => {
+  // Add data to the queue
+}
+
+const updateStatus = (id, newStatus) => {
+  // Change the data in queue based on the id and newStatus
+}
+
+const removeFromQueue = (id) => {
+  // Remove data from the queue based on the id
+}
+
   return (
     <div>
       <header>
         <h1>Queue Management System</h1>
         <p>Manage your customers efficiently</p>
       </header>
+      <main>
+        <QueueForm onAdd={addToQueue} />
+        <h1>Queue Display</h1>
+      </main>
       
     </div>
   );
