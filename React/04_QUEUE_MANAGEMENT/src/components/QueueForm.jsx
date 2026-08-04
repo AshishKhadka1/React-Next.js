bd2cfd637cb9be7d5feb740678b349628060f59d
AshishKhadka <khadkaashish50@gmail.com>
2026-08-04 23:08:25 +0545

    updated

--- React/04_QUEUE_MANAGEMENT/src/components/QueueForm.jsx
+++ React/04_QUEUE_MANAGEMENT/src/components/QueueForm.jsx
@@ -1,3 +1,42 @@
+import { useState } from "react";
+import {FaUserPlus} from "react-icons/fa"
+
 export default function QueueForm(onAdd) {
-    return 
+  const [name, setName] = useState("");
+  const [service, setService] = useState("");
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      // Validations
+      if (!name.trim() || !service.trim()) return;
+      onAdd({ name, service });
+      setName("");
+      setService("");
+    };
+  return (
+    <>
+      <form className="queue-form" onSubmit={handleSubmit}>
+        <h2>Add to Queue</h2>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Customer Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <select value={service} onChange={(e) => setService(e.target.value)}>
+            <option value="">Select Service</option>
+            <option value="consultation">Consultation</option>
+            <option value="support">Support</option>
+            <option value="billing">Billing</option>
+          </select>
+        </div>
+        <button type="submit">
+            <FaUserPlus/> Add Customer
+        </button>
+      </form>
+    </>
+  );
 }
